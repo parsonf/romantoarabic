02ada6655b02380452f5e4e7d9d16eac1f7adcfe
parsonf <brianparsonf@gmail.com>
2015-03-29 15:14:55 -0500

    Hello world, built bare bones framework to make runnable JAR with java -jar.

---
+++ src/edu/compilers/teamb/RomanToArabicMain.java
@@ -0,0 +1,12 @@
+package edu.compilers.teamb;
+
+public class RomanToArabicMain {
+
+    public RomanToArabicMain() {
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        new RomanToArabicMain();
+    }
+}
