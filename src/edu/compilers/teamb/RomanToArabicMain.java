befcafa7fca8cc5197592594289060912799f7ff
parsonf <brianparsonf@gmail.com>
2015-03-29 19:15:35 -0500

    Main program loop is in place.

--- src/edu/compilers/teamb/RomanToArabicMain.java
+++ src/edu/compilers/teamb/RomanToArabicMain.java
@@ -1,12 +1,42 @@
 package edu.compilers.teamb;
 
-public class RomanToArabicMain {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RomanToArabicMain implements Runnable {
+    private String input;
 
     public RomanToArabicMain() {
-        System.out.println("hello world");
+        input = "";
+    }
+
+    public void run() {
+        startMessage();
+        // main program loop
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        do {
+            System.out.print("RtA> ");
+            try {
+                input = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+            processCommand(input);
+        } while (!(input.equalsIgnoreCase("exit")));
+    }
+
+    private void processCommand(String input) {
+
+    }
+
+    private void startMessage() {
+        System.out.println("Welcome!");
+        System.out.println("Here's a list of commands or something. Go.");
     }
 
     public static void main(String[] args) {
-        new RomanToArabicMain();
+        (new Thread(new RomanToArabicMain())).start();
     }
 }
